cd65a4beedf5c9b185d02afbcc5272e98681613b
yangguangpeng <l>
2019-11-24 11:38:14 +0800

    初始

---
+++ src/Http/Controllers/JunitController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Perry\Junit\Http\Controllers;
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+class JunitController extends Controller{
+
+    public function index()
+    {
+        $r = view();
+        return view('junit::index');// ?? view(??)
+    }
+
+    public function store(Request $request)
+    {
+        // 用反射去创建实例类，缓存记录....
+        $namespace  = $request->input('namespace');
+        $className  = $request->input('className');
+        $action     = $request->input('action');
+        $param      = $request->input('param');
+        $class = ($className == "") ? $namespace : $namespace.'\\'.$className;
+        // 要提换的值  需要的结果
+        $class = str_replace("/", "\\", $class);
+        $object = new $class();// 创建实际的类
+        $param = ($param == "") ? [] : explode('|', $param) ;// 参数的处理
+        $data = call_user_func_array([$object, $action], $param);// 最终的执行
+        return (is_array($data)) ? json_encode($data) : '';
+    }
+
+}
